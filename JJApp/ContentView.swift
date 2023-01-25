d8bcea74855981d71e186e33ebd81ca51a3bbe63
Arnella Tolegen <arnellatolegen@MacBook-Pro-Arnella.local>
2023-01-25 17:41:59 +0600

    fast commit

---
+++ JJApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  JJApp
+//
+//  Created by Arnella Tolegen on 25.01.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ JJApp/JJAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  JJAppApp.swift
+//  JJApp
+//
+//  Created by Arnella Tolegen on 25.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct JJAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
